4103f2b5cb09d131f00c9eb81d8edb2f21fab88b
Tales Palma <tales.palma_2022@outlook.com>
2024-07-05 17:43:27 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/tales")]
+async fn tales_rota() -> impl Responder {
+    HttpResponse::Ok().body("Tales lindo e maravilhoso")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .service(tales_rota)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
